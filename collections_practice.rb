9d66b74d71ea5a588c5be00233a15ecb23308f7d
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-23 15:09:17 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,71 @@
-# your code goes here
+def begins_with_r(array)
+  i=0
+  while i < array.length
+    if array[i][0] != 'r'
+      return false
+    end
+    i +=1
+
+  end
+  true
+end
+
+def contain_a(array)
+  array2 = []
+  array.each do |x|
+    if x.include?("a")
+      array2 << x
+    end
+  end
+
+  array2
+end
+
+def first_wa(array)
+
+  array.each do |word|
+    if word[0..1] == 'wa'
+      return word
+    end
+  end
+end
+
+def remove_non_strings(array)
+
+  array.select do |word|
+    word.instance_of? String
+  end
+end
+
+def count_elements(array, element)
+  counter = 0
+  array.each do |x|
+    if x == element
+      counter+=1
+    end
+  end
+  counter
+
+end
+
+def merge_data(array, array2)
+  array << array2
+end
+
+def find_cool(hash)
+  hash.each_with_index do |x,i|
+    hash[i] == 'cool'
+  end
+end
+
+def organize_schools(array)
+  array.sort do |a,b|
+    if a==b
+      0
+    elsif a < b
+      -1
+    elsif a > b
+      1
+    end
+  end
+end
